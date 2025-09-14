7fcfa9790dcc5b7f96d10078000921709c616a6c
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2025-09-15 00:21:31 +0530

    Apireasponse

---
+++ src/utils/Apireasponse.js
@@ -0,0 +1,9 @@
+class Apireasponse{
+    constructor(statusCode, message="success", data){
+        this.status = statusCode;
+        this.message = message;
+        this.data = null;
+        this.success = statusCode >= 200 && statusCode < 300 ? true : false;
+    }
+}
+module.exports = Apireasponse;
