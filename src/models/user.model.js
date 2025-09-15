d99cccc309c4d7dad355dae8e478fdfc688ad5bb
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2025-09-16 01:27:25 +0530

    jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -63,5 +63,37 @@ userSchema.pre('save',async function (next){// arrow function not used to access
 userSchema.methods.commparePassword = async function(candidatePassword){// candidatePassword is the password entered by the user during login
     return await bcrypt.compare(candidatePassword,this.password);// true or false
 }
+// Generate JWT token
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+    {
+        _id: this._id,
+        username: this.username,
+        email:this.email,
+        fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECERET,
+
+    {
+       expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+     return jwt.sign(
+    {
+        _id: this._id
+    },
+    process.env.REFERESH_TOKEN_SECERET,
+
+    {
+       expiresIn: process.env.REFERESH_TOKEN_EXPIRY
+    }
+
+    )
+}
+
 const User = mongoose.model('User', userSchema);
 export default User;
