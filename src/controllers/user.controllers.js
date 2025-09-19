d783f0b554c98cf2161a1500c1448bd3eaf40735
singhshubham103codeclub <shubhamsingh22485@gmail.com>
2025-09-20 03:30:22 +0530

    createUserregitation algo

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,8 +2,13 @@ import {asyncHandler} from  "../utils/asyncHandler.js"
 
 
 const registerUser=asyncHandler(async(req,rep)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+   //get user details from frontend
+   //validation- not empaty
+   //check user if user already exits: username and email
+   //check for image ,chech avatar
+   //upload them cloudinary ,avatar
+   // create user object-create entry in db
+   // remove password and referesh tocken field from response
+   // return response
 })
 export {registerUser}
